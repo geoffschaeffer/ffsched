51ed87a5d80f9cc80f71a8287a345713e8c66234
Geoff Schaeffer <geoffrey.schaeffer@sv.cmu.edu>
2012-09-19 21:22:29 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ffsched::Application.config.secret_token = '2395427679326fddcbf7ac7c6a16f4d28626ed2961d2fcc472127a67f89b6e01918cbff52f85ccd7e63192d47646d88272af6434e65798b8c734c9558cafd7e3'
