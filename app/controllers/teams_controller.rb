6f5079caf81009e7a7163e4b7a9b2776953e445a
Geoff Schaeffer <geoffrey.schaeffer@sv.cmu.edu>
2012-09-22 13:58:25 -0700

    Finishing basic team stuff

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,9 +1,46 @@
 class TeamsController < ApplicationController
 
-  def new
+  def index
+    @teams = Team.all
   end
 
   def show
     @team = Team.find(params[:id])
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(params[:team])
+    if @team.save
+      # Handle a successful save.
+      flash[:success] = "Created Team!"
+      redirect_to teams_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @team = Team.find(params[:id])
+  end
+
+  def update
+    @team = Team.find(params[:id])
+    if @team.update_attributes(params[:team])
+      # Handle a successful update.
+      flash[:success] = "Team updated"
+      redirect_to teams_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Team.find(params[:id]).destroy
+    flash[:success] = "Team destroyed."
+    redirect_to teams_path
   end
 end
