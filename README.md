a9a8953789ea588ee35d58f23880ee730522bbab
Geoff Schaeffer <geoffrey.schaeffer@sv.cmu.edu>
2012-09-21 21:42:40 -0700

    Adding create for leagues

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 by Geoff Schaeffer
 This application allows someone to schedule a fantasy football league.
 (Currently, it only works with a 14-team league playing a 13-week season.)
+
+This app relies heavily on code and examples from Michael Hartl's Ruby on Rails tutorial.
--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,6 +1,18 @@
 class LeaguesController < ApplicationController
 
+  def create
+    @league = League.new(params[:league])
+    if @league.save
+      # Handle a successful save.
+      flash[:success] = "Created League!"
+      redirect_to @league
+    else
+      render 'new'
+    end
+  end
+
   def new
+    @league = League.new
   end
 
   def show
