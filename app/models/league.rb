260c832555318c53e4877cd4c4e36db43b933201
Geoff Schaeffer <geoffrey.schaeffer@sv.cmu.edu>
2012-09-22 22:51:31 -0700

    Fix views don't show add button when 14 teams reached

--- app/models/league.rb
+++ app/models/league.rb
@@ -3,4 +3,15 @@ class League < ActiveRecord::Base
   has_many :teams, dependent: :destroy
 
   validates :name,  presence: true, length: { maximum: 25 }, uniqueness: true
+
+  def has_room?
+    return teams.length < 14
+  end
+
+  private
+
+    def num_teams_in_bounds
+      return if teams.blank?
+      errors.add("Teams in a league limited to #{@max_teams}") if teams.length > 14
+    end
 end
