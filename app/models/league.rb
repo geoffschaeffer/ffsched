5cd9034b1cee26d099835422ebec9d1f69c475d9
Geoff Schaeffer <geoffrey.schaeffer@sv.cmu.edu>
2012-09-23 23:57:02 -0700

    Fix up league#show to interleave teams

--- app/models/league.rb
+++ app/models/league.rb
@@ -4,14 +4,26 @@ class League < ActiveRecord::Base
 
   validates :name,  presence: true, length: { maximum: 25 }, uniqueness: true
 
+  def get_max_teams
+    return 14
+  end
+
   def has_room?
-    return teams.length < 14
+    return teams.length < get_max_teams
+  end
+
+  def team_index_even?(team)
+    (teams.index(team) % 2) == 0
+  end
+
+  def team_index_last(team)
+    teams.index(team) == (teams.length - 1)
   end
 
   private
 
     def num_teams_in_bounds
       return if teams.blank?
-      errors.add("Teams in a league limited to #{@max_teams}") if teams.length > 14
+      errors.add("Teams in a league limited to #{get_max_teams}") if teams.length > get_max_teams
     end
 end
