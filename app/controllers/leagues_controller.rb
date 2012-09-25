1ff382657841a71048646c7a7f7535e87ab2ca1c
Geoff Schaeffer <geoffrey.schaeffer@sv.cmu.edu>
2012-09-25 00:05:43 -0700

    Working on leagues REST XML API

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -10,29 +10,41 @@ class LeaguesController < ApplicationController
   def show
     @league = League.find(params[:id])
     @teams = @league.teams
+    respond_with(@league)
   end
 
   def new
     @league = League.new
+    respond_with(@league)
   end
 
   def create
     @league = League.new(params[:league])
-    if @league.save
-      # Handle a successful save.
-      flash[:success] = "Created League!"
-      redirect_to leagues_path
-    else
-      render 'new'
+
+    respond_to do |format|
+      if @league.save
+        format.html {
+          flash[:success] = "Created League!"
+          redirect_to leagues_path
+        }
+        format.xml  { render xml: @league, status: :created, location: @person }
+      else
+        format.html { render 'new' }
+        format.xml { render xml: @league.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def edit
     @league = League.find(params[:id])
+    #TODO - is this correct?
+    respond_with(@league)
   end
 
   def update
     @league = League.find(params[:id])
+
+    #TODO - rock this on Tues
     if @league.update_attributes(params[:league])
       # Handle a successful update.
       flash[:success] = "League updated"
